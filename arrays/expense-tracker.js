3c09b5103ac06740ad824b13af89cae1bf951186
diego fontecilla <mail@diegofontecilla.com>
2018-12-17 12:48:20 +0000

    Added income and accountBalance to expense-tracker.js

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,22 +1,39 @@
 const account = {
     name: 'Diego Fontecilla',
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         this.expenses.push({
             description: description, 
             amount: amount 
         })
     }, 
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
     getAccountSummary: function () {
         let totalExpenses = 0
+        let totalIncome = 0
+        let accountBalance = 0
 
         this.expenses.forEach(function (expense) {
             totalExpenses += expense.amount
         })
-        return `${this.name} has $${totalExpenses} in expenses.`
-}
+
+        this.income.forEach(function (income) {
+            totalIncome += income.amount
+        })
+
+        accountBalance = totalIncome - totalExpenses
+
+        return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses.`
+    }
 }
 
-account.addExpense('Car', 2000)
-account.addExpense('Coffee', 2)
+account.addExpense('Food', 20)
+account.addExpense('Coffee', 4)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
