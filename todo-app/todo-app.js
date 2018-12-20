ba7cab110c6d32d649e013e4dd47199af5ee7ec2
diego fontecilla <mail@diegofontecilla.com>
2018-12-20 21:43:19 +0000

    Added simple form to todo and notes apps, preventing default. On todo, adding new tod to the list and render

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -43,17 +43,19 @@ const renderTodos = function (todos, filters) {
 
 renderTodos(todos, filters)
 
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('I am adding a new todo')
-})
-
-document.querySelector('#new-todo-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
-})
-
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value, 
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
 })
 
 
